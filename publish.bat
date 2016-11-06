934e11a0ae7076c9ef007e1a8cfd421bca6d0dc7
jindraivanek <jindra.ivanek@gmail.com>
2016-11-06 20:20:47 +0100

    snippets collection

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@
 
 call .paket\paket restore
 call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
-call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code/snippets" --outputDirectory "_snippets"
+call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "snippets" --outputDirectory "_snippets"
 
 git add --all .
 git commit -a -m %1
