090cae2abaccf40c2919e9e308786a1f9aba2ac9
jindraivanek <jindra.ivanek@gmail.com>
2016-11-06 19:22:48 +0100

    test post update

--- code/2016-11-06-test.fsx
+++ code/2016-11-06-test.fsx
@@ -5,7 +5,7 @@ title: test post
 ---
 (**
 ##Introduction
-F# is a mature, open source, cross-platform, functional-first programming language. It empowers users and organizations to tackle complex computing problems with simple, maintainable and robust code.
+[F#](https://fsharp.org/) is a mature, open source, cross-platform, functional-first programming language. It empowers users and organizations to tackle complex computing problems with simple, maintainable and robust code.
 *)
 
 let safeTake n s = 
