cef66babebc45fc5e7be45925988be754f78008f
jindraivanek <jindra.ivanek@gmail.com>
2016-11-06 19:03:51 +0100

    theme update

--- code/2016-11-06-test.fsx
+++ code/2016-11-06-test.fsx
@@ -8,4 +8,6 @@ title: test post
 F# is a mature, open source, cross-platform, functional-first programming language. It empowers users and organizations to tackle complex computing problems with simple, maintainable and robust code.
 *)
 
-let x = 1
+let safeTake n s = 
+    let x = ref n
+    Seq.takeWhile (fun _ -> x := !x - 1; !x >= 0) s
