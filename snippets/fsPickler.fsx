1c609ea4eb953a90f412ca3539d4b6c230da2561
Jindra Ivanek <jindra.ivanek@gmail.com>
2017-01-25 10:52:35 +0100

    fsPickler snippet

---
+++ snippets/fsPickler.fsx
@@ -0,0 +1,13 @@
+(*** raw ***)
+---
+layout: page
+title: Memoize
+---
+(** FsPickler can be used to "serialize" functions: *)
+open MBrace.FsPickler
+open MBrace.FsPickler.Json
+
+let s = FsPickler.CreateJsonSerializer()
+let pickle = s.PickleToString (fun x -> x + 1)
+let f:int->int = bs.UnPickleOfString pickle
+f(1)
